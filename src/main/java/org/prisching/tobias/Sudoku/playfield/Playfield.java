01f8d75e6ceab3b62f3330276fdf6eaafce0188e
Tobias Prisching <tobias.prisching@icloud.com>
2021-12-03 01:13:48 +0100

    Update Playfield
    Added constants for X and Y dimensions and method to get the map of all fields

--- src/main/java/org/prisching/tobias/Sudoku/playfield/Playfield.java
+++ src/main/java/org/prisching/tobias/Sudoku/playfield/Playfield.java
@@ -5,7 +5,11 @@ import java.util.HashMap;
 
 public class Playfield {
 
+	// Should contain 81 fields with positions from (0,0) to (8,8)
 	private Map<Position, SudokuField> fields;
+	
+	public static final int MAX_X = 9;
+	public static final int MAX_Y = 9;
 	
 	public Playfield() {
 		this.fields = new HashMap<Position, SudokuField>();
@@ -17,6 +21,10 @@ public class Playfield {
 	
 	public SudokuField getField (Position pos) {
 		return this.fields.values().stream().filter(f -> f.getPos().equals(pos)).findFirst().get();
+	}
+	
+	public Map<Position, SudokuField> getAllFields() {
+		return this.fields;
 	}
 	
 }
