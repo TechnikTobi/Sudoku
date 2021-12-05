33924510671ba8896738ab1a774bf4bf27672056
Tobias Prisching <tobias.prisching@icloud.com>
2021-12-06 00:11:58 +0100

    Update non-abstract Requests
    - Made instance variables final
    - Extracted JSON field names
    - Implement abstract getPrintString Method

--- src/main/java/org/prisching/tobias/Sudoku/messages/incoming/GameCreationRequest.java
+++ src/main/java/org/prisching/tobias/Sudoku/messages/incoming/GameCreationRequest.java
@@ -7,35 +7,54 @@ import javax.validation.constraints.Max;
 
 import org.prisching.tobias.Sudoku.game.player.PlayerID;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class GameCreationRequest extends Request {
+
+	private static final String PLAYER_ID = "PlayerID";
+	private static final String GAME_NAME = "GameName";
+	private static final String DIFFICULTY = "Difficulty";
 
 	@NotNull
 	@Size(min = 1)
-	private String playerIDstring;
-	
+	private final String playerIDstring;
+
 	@NotNull
 	@Size(min = 1)
-	private String gameName;
-	
+	private final String gameName;
+
 	@Min(0)
 	@Max(81)
-	private int difficulty;
-	
-	public GameCreationRequest(String playerIDstring, String gameName, int difficulty) {
+	private final int difficulty;
+
+	@JsonCreator
+	public GameCreationRequest(@JsonProperty(value = PLAYER_ID, required = true) String playerIDstring, @JsonProperty(value = GAME_NAME, required = true) String gameName, @JsonProperty(value = DIFFICULTY, required = true) int difficulty) {
 		this.playerIDstring = playerIDstring;
 		this.gameName = gameName;
 		this.difficulty = difficulty;
 	}
-	
+
 	public PlayerID getPlayerID() {
 		return new PlayerID(this.playerIDstring);
 	}
-	
-	public String getName() {
+
+	public String getGameName() {
 		return this.gameName;
 	}
-	
+
 	public int getDifficulty() {
 		return this.difficulty;
+	}
+
+	@Override
+	@JsonIgnore
+	public String getPrintString() {
+		return 
+			this.getClass().getName() + ": "
+				+ "'" + PLAYER_ID + "'='" + this.playerIDstring + "'"
+				+ "'" + GAME_NAME + "'='" + this.gameName + "'"
+				+ "'" + DIFFICULTY + "'='" + this.difficulty + "'";
 	}
 }
--- src/main/java/org/prisching/tobias/Sudoku/messages/incoming/PlayerRegistrationRequest.java
+++ src/main/java/org/prisching/tobias/Sudoku/messages/incoming/PlayerRegistrationRequest.java
@@ -4,21 +4,30 @@ import javax.validation.constraints.Size;
 import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class PlayerRegistrationRequest extends Request {
 
+	private static final String PLAYER_NAME = "PlayerName";
+	
 	@NotNull
 	@Size(min = 3, max = 100)
-	private String name;
+	private final String playerName;
 	
 	@JsonCreator
-	public PlayerRegistrationRequest(@JsonProperty(value = "playerName", required = true) String name) {
-		this.name = name;
+	public PlayerRegistrationRequest(@JsonProperty(value = PLAYER_NAME, required = true) String name) {
+		this.playerName = name;
 	}
 	
-	public String getName() {
-		return this.name;
+	public String getPlayerName() {
+		return this.playerName;
+	}
+
+	@Override
+	@JsonIgnore
+	public String getPrintString() {
+		return this.getClass().getName() + ": '" + PLAYER_NAME + "'='" + this.playerName + "'";
 	}
 	
 }
