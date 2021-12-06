cff85fa3de19d5e93f9a723b24f54fc625c2cfa6
Tobias Prisching <tobias.prisching@icloud.com>
2021-12-07 00:47:40 +0100

    Created JSONnames
    Class used to store public static final Strings of the names of variables when converted to JSON in a central place.

---
+++ src/main/java/org/prisching/tobias/Sudoku/messages/base/JSONnames.java
@@ -0,0 +1,12 @@
+package org.prisching.tobias.Sudoku.messages.base;
+
+public class JSONnames {
+
+	public static final String GAME_ID = "GameID";
+	public static final String PLAYER_ID = "PlayerID";
+	
+	public static final String PLAYER_NAME = "PlayerName";
+	
+	public static final String GAME_NAME = "GameName";
+	public static final String DIFFICULTY = "Difficulty";
+}
