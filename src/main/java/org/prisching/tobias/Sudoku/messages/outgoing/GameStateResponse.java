32f5ee80cb47cef1b1d7f038cbf85edc94a28866
Tobias Prisching <tobias.prisching@icloud.com>
2021-12-07 00:54:33 +0100

    Rename of GameState to GameStateResponse + Updates
    To keep naming consistent here as well. Also added nested PlayerListElement class for the list of players with their points.

---
+++ src/main/java/org/prisching/tobias/Sudoku/messages/outgoing/GameStateResponse.java
@@ -0,0 +1,69 @@
+package org.prisching.tobias.Sudoku.messages.outgoing;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.annotation.JsonGetter;
+
+import org.prisching.tobias.Sudoku.board.Field;
+import org.prisching.tobias.Sudoku.game.GameController;
+import org.prisching.tobias.Sudoku.game.player.Player;
+import org.prisching.tobias.Sudoku.game.player.PlayerColor;
+import org.prisching.tobias.Sudoku.game.player.PlayerID;
+import org.prisching.tobias.Sudoku.game.player.PlayerManager;
+
+public class GameStateResponse extends Response {
+
+	private GameController gameController;
+	private PlayerManager playerManager;
+	
+	public class PlayerListElement {
+		
+		private final Player player;
+		private final int points;
+		
+		public PlayerListElement(Player player, int points) {
+			this.player = player;
+			this.points = points;
+		}
+		
+		public String getName() {
+			return this.player.getName();
+		}
+		
+		public int getPoints() {
+			return this.points;
+		}
+		
+		public PlayerColor getColor() {
+			return this.player.getColor();
+		}
+		
+	}
+	
+	public GameStateResponse(GameController gameController, PlayerManager playerManager) {
+		this.gameController = gameController;
+		this.playerManager = playerManager;
+	}
+	
+	public GameStateResponse(GameController gameController, PlayerManager playerManager, String message) {
+		this(gameController, playerManager);
+		this.setMessage(message);
+	}
+	
+	@JsonGetter("fields")
+	public List<Field> getFields() {
+		return this.gameController.getBoard().getAllFields().values().stream().collect(Collectors.toList());
+	}
+	
+	@JsonGetter("players")
+	public List<PlayerListElement> getPlayers() {
+		return this.gameController.getPoints()
+			.entrySet()
+			.stream()
+			.map(e -> new PlayerListElement(this.playerManager.getPlayer(e.getKey()), e.getValue().intValue()))
+			.collect(Collectors.toList());
+	}
+	
+}
