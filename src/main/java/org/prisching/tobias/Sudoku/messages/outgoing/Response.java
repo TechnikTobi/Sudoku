3de45384ddb2312180dc215ae2e53286306869a5
Tobias Prisching <tobias.prisching@icloud.com>
2021-12-07 12:16:02 +0100

    Update Response
    Only include the message if its not null

--- src/main/java/org/prisching/tobias/Sudoku/messages/outgoing/Response.java
+++ src/main/java/org/prisching/tobias/Sudoku/messages/outgoing/Response.java
@@ -1,10 +1,12 @@
 package org.prisching.tobias.Sudoku.messages.outgoing;
 
 import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 public abstract class Response {
 
-	private String message = "";
+	@JsonInclude(JsonInclude.Include.NON_NULL) 
+	private String message = null;
 	
 	protected void setMessage(String message) {
 		this.message = message;
