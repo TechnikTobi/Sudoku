9a48ac41812900d893276f02163f3f571dd6fc98
Tobias Prisching <tobias.prisching@icloud.com>
2021-12-07 12:24:40 +0100

    Update JSONnames
    More JSON keys

--- src/main/java/org/prisching/tobias/Sudoku/messages/base/JSONnames.java
+++ src/main/java/org/prisching/tobias/Sudoku/messages/base/JSONnames.java
@@ -6,7 +6,13 @@ public class JSONnames {
 	public static final String PLAYER_ID = "PlayerID";
 	
 	public static final String PLAYER_NAME = "PlayerName";
-	
 	public static final String GAME_NAME = "GameName";
+	
+	public static final String GAMES = "Games";
 	public static final String DIFFICULTY = "Difficulty";
+	public static final String MASTER_ID = "MasterID";
+	public static final String MASTER_NAME = "MasterName";
+	
+	public static final String DATA = "Data";
+	public static final String ERROR = "ErrorMessage";
 }
