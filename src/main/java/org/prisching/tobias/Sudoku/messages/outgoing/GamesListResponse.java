6cebd3d339d34094085dc2a8cf4fbd38a88a3fff
Tobias Prisching <tobias.prisching@icloud.com>
2021-12-08 14:55:56 +0100

    Update GamesListResponse
    Extracted GamesListElement into own class, working on making this independent from server internal stuff.

--- src/main/java/org/prisching/tobias/Sudoku/messages/outgoing/GamesListResponse.java
+++ src/main/java/org/prisching/tobias/Sudoku/messages/outgoing/GamesListResponse.java
@@ -3,60 +3,15 @@ package org.prisching.tobias.Sudoku.messages.outgoing;
 import java.util.List;
 import java.util.ArrayList;
 
-import org.prisching.tobias.Sudoku.game.GameID;
 import org.prisching.tobias.Sudoku.game.GameController;
 import org.prisching.tobias.Sudoku.game.player.Player;
-import org.prisching.tobias.Sudoku.game.player.PlayerID;
+import org.prisching.tobias.Sudoku.messages.base.GamesListElement;
 import org.prisching.tobias.Sudoku.messages.base.JSONnames;
+import org.prisching.tobias.Sudoku.messages.base.NetworkGameIdentifier;
 
 import com.fasterxml.jackson.annotation.JsonGetter;
 
 public class GamesListResponse extends Response {
-
-	public class GamesListElement {
-
-		private GameID gameID;
-		private String gameName;
-		
-		private PlayerID masterID;
-		private String masterName;
-		
-		private int difficulty;
-
-		public GamesListElement(GameID gameID, String gameName, PlayerID masterID, String masterName, int difficulty) {
-			this.gameID = gameID;
-			this.gameName = gameName;
-			this.masterID = masterID;
-			this.masterName = masterName;
-			this.difficulty = difficulty;
-		}
-		
-		@JsonGetter(JSONnames.GAME_ID)
-		public String getGameID() {
-			return this.gameID.getGameID();
-		}
-		
-		@JsonGetter(JSONnames.GAME_NAME)
-		public String getGameName() {
-			return this.gameName;
-		}
-		
-		@JsonGetter(JSONnames.MASTER_ID)
-		public String getMasterID() {
-			return this.masterID.getPlayerID();
-		}
-		
-		@JsonGetter(JSONnames.MASTER_NAME)
-		public String getMasterName() {
-			return this.masterName;
-		}
-		
-		@JsonGetter(JSONnames.DIFFICULTY)
-		public int getDifficulty() {
-			return this.difficulty;
-		}
-		
-	}
 	
 	private List<GamesListElement> list;
 	
@@ -66,7 +21,7 @@ public class GamesListResponse extends Response {
 		for(GameController game : games) {
 			if(game.isJoinable()) {
 				String masterName = players.stream().filter(p -> p.getID().equals(game.getMaster())).findFirst().get().getName();
-				this.list.add(new GamesListElement(game.getID(), game.getName(), game.getMaster(), masterName, game.getDifficulty()));
+				this.list.add(new GamesListElement(new NetworkGameIdentifier(game.getID().getGameID()), game.getName(), masterName, game.getDifficulty(), game.countReadyPlayers(), game.countTotalPlayers()));
 			}
 		}
 	}
