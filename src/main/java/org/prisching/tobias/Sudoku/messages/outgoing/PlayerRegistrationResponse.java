7ba7cb0c1411110e19f9e285a5529ab882794043
Tobias Prisching <tobias.prisching@icloud.com>
2021-12-10 10:42:32 +0100

    Update PlayerRegistrationResponse
    Now uses NetworkPlayerIdentifier instead of PlayerID

--- src/main/java/org/prisching/tobias/Sudoku/messages/outgoing/PlayerRegistrationResponse.java
+++ src/main/java/org/prisching/tobias/Sudoku/messages/outgoing/PlayerRegistrationResponse.java
@@ -1,26 +1,26 @@
 package org.prisching.tobias.Sudoku.messages.outgoing;
 
-import org.prisching.tobias.Sudoku.game.player.PlayerID;
 import org.prisching.tobias.Sudoku.messages.base.JSONnames;
+import org.prisching.tobias.Sudoku.messages.base.NetworkPlayerIdentifier;
 
 import com.fasterxml.jackson.annotation.JsonGetter;
 
 public class PlayerRegistrationResponse extends Response {
 
-	private PlayerID id;
+	private NetworkPlayerIdentifier netPlayerID;
 	
-	public PlayerRegistrationResponse(PlayerID id) {
-		this.id = id;
+	public PlayerRegistrationResponse(NetworkPlayerIdentifier netPlayerID) {
+		this.netPlayerID = netPlayerID;
 	}
 	
 	public PlayerRegistrationResponse(String message) {
-		this.id = null;
+		this.netPlayerID = null;
 		this.setMessage(message);
 	} 
 	
 	@JsonGetter(JSONnames.PLAYER_ID)
 	public String getPlayerIDstring() {
-		return this.id.getPlayerID();
+		return this.netPlayerID.getIdentifier();
 	}
 	
 }
