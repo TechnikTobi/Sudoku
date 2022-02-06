b95497a8a48d4c171fed36024c9855e13205dc21
Tobias Prisching <tobias.prisching@icloud.com>
2022-02-07 00:20:09 +0100

    Added JavaDoc for Position

--- src/main/java/org/prisching/tobias/Sudoku/board/Position.java
+++ src/main/java/org/prisching/tobias/Sudoku/board/Position.java
@@ -1,36 +1,63 @@
 package org.prisching.tobias.Sudoku.board;
 
+/**
+ * The Position class is a simple data structure for storing the coordinates e.g. of a {@link Field} object
+ */
 public class Position {
 
-	private int x;
-	private int y;
-	
+	/** The x coordinate of the position */
+	private final int x;
+
+	/** The y coordinate of the position */
+	private final int y;
+
+	/**
+	 * Constructor for creating a new position
+	 * @param x The x coordinate of the new position
+	 * @param y The y coordinate of the new position
+	 */
 	public Position(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
-	
+
+	/**
+	 * Constructor for creating a new, empty Position
+	 */
 	public Position() {
 		this(0, 0);
 	}
-	
+
+	/**
+	 * Gets the x coordinate of the position
+	 * @return The x coordinate
+	 */
 	public int getX() {
 		return this.x;
 	}
-	
+
+	/**
+	 * Gets the y coordinate of the position
+	 * @return The y coordinate
+	 */
 	public int getY() {
 		return this.y;
 	}
-	
+
+	/**
+	 * Method for printing a position. Useful for debugging.
+	 */
 	public void print() {
 		System.out.println("(" + this.x + "," + this.y + ")");
 	}
 
+	/** {@inheritDoc} */
 	@Override
 	public int hashCode() {
 		return this.x * 11 + this.y * 31;
 	}
-	
+
+	/** {@inheritDoc} */
 	@Override
 	public boolean equals(Object obj) {		
 		if (this == obj)
