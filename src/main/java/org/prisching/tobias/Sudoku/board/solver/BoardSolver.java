ce9f863f60bf46ca419aa163ddcf2212b58ee4ce
Tobias Prisching <tobias.prisching@icloud.com>
2022-02-06 12:19:30 +0100

    Refactoring of the BoardSolver + Added JavaDoc

--- src/main/java/org/prisching/tobias/Sudoku/board/solver/BoardSolver.java
+++ src/main/java/org/prisching/tobias/Sudoku/board/solver/BoardSolver.java
@@ -11,48 +11,63 @@ import org.prisching.tobias.Sudoku.board.Field;
 import org.prisching.tobias.Sudoku.board.Position;
 import org.prisching.tobias.Sudoku.board.validation.BoardValidator;
 
+/**
+ * The BoardSolver provides a static method for solving a given Sudoku Board
+ */
 public class BoardSolver {
 
+	/**
+	 * Tries to find all possible solutions for a given Sudoku
+	 * @param board The board of the Sudoku to solve
+	 * @return The number of solutions that were found
+	 */
 	public static int solve(Board board) {
 		return BoardSolver.recursiveSolve(board);
 	}
-	
+
+	/**
+	 * Recursively solves a Sudoku by filling in empty fields of the board as long as the Sudoku is valid
+	 * @param board The board of the Sudoku to solve
+	 * @return The number of solutions that were found
+	 */
 	private static int recursiveSolve(Board board) {
 	
 		BoardInfoExtractor extractor = new BoardInfoExtractor(board);
-		
-		if(BoardValidator.getValidator().validate(board) == false) return 0;
+
+		// If the board is invalid, return 0. If it is already full (i.e. no more fields to fill) return 1
+		if(!BoardValidator.getValidator().validate(board)) return 0;
 		if(extractor.isFull()) return 1;
-		
-		for(int i = 0; i < Board.MAX_X*Board.MAX_Y; i++) {
-			
-			int x = i % Board.MAX_X;
-			int y = i / Board.MAX_Y;
-			Position pos = new Position(x, y); 
-			
-			if(board.getField(pos).getValue() != Field.EMPTY_FIELD_VALUE) continue;
-			
-			int returnValue = 0;
-			List<Integer> numbers = IntStream.range(1, 10).boxed().collect(Collectors.toList());
-			Collections.sort(numbers);
-			
-			for(Integer value : numbers) {
-				
-				if(extractor.getRow(pos).stream().filter(f -> f.getValue() == value).findAny().isPresent()) continue;
-				if(extractor.getColumn(pos).stream().filter(f -> f.getValue() == value).findAny().isPresent()) continue;
-				if(extractor.getSquare(pos).stream().filter(f -> f.getValue() == value).findAny().isPresent()) continue;
-				
-				board.getField(pos).setValue(value);
-				returnValue += BoardSolver.recursiveSolve(board);
-				board.getField(pos).setValue(Field.EMPTY_FIELD_VALUE);
-				
-			}
-			
-			return returnValue;
+
+		// Get the position of the first empty field you can find
+		Position emptyPosition = board.getAllFields().values()
+				.stream()
+				.filter(f -> f.getValue() == Field.EMPTY_FIELD_VALUE)
+				.map(Field::getPos)
+				.findFirst().orElse(null);
+
+		assert(emptyPosition != null);
+
+		int returnValue = 0;
+		List<Integer> numbers = IntStream.range(1, 10).boxed().sorted().toList();
+
+		for(Integer value : numbers) {
+
+			// If the value is already present within the row, column or square, use the next number
+			if(extractor.getRow(emptyPosition).stream().anyMatch(f -> f.getValue() == value)) continue;
+			if(extractor.getColumn(emptyPosition).stream().anyMatch(f -> f.getValue() == value)) continue;
+			if(extractor.getSquare(emptyPosition).stream().anyMatch(f -> f.getValue() == value)) continue;
+
+			// If all these checks pass, set the field accordingly and continue solving the board by making a recursive
+			// call to solve the next empty field
+			board.getField(emptyPosition).setValue(value);
+			returnValue += BoardSolver.recursiveSolve(board);
+
+			// Reset the value for the field once done
+			board.getField(emptyPosition).setValue(Field.EMPTY_FIELD_VALUE);
+
 		}
-		
-		return 0;
-		
+
+		return returnValue;
 	}
 	
 }
