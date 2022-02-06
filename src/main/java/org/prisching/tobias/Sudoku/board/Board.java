2fed15e58dd0284ba89ca3e717f13f75dae99c39
Tobias Prisching <tobias.prisching@icloud.com>
2022-02-07 00:21:00 +0100

    Added JavaDoc for Board

--- src/main/java/org/prisching/tobias/Sudoku/board/Board.java
+++ src/main/java/org/prisching/tobias/Sudoku/board/Board.java
@@ -3,16 +3,25 @@ package org.prisching.tobias.Sudoku.board;
 import java.util.Map;
 import java.util.HashMap;
 
+/**
+ * The Board class is the data structure for holding a Sudoku board with all of its fields
+ */
 public class Board {
 
-	// Should contain 81 fields with positions from (0,0) to (8,8)
-	private Map<Position, Field> fields;
-	
+	/** The list of all fields with positions ranging from (0,0) to (8,8) */
+	private final Map<Position, Field> fields;
+
+	/** Number of fields in the x direction */
 	public static final int MAX_X = 9;
+
+	/** Number of fields in the y direction */
 	public static final int MAX_Y = 9;
-	
+
+	/**
+	 * Constructor for creating a new, empty board
+	 */
 	public Board() {
-		this.fields = new HashMap<Position, Field>();
+		this.fields = new HashMap<>();
 		for(int y = 0; y < MAX_Y; y++) {
 			for(int x = 0; x < MAX_X; x++) {
 				Position newPos = new Position(x, y);
@@ -20,9 +29,13 @@ public class Board {
 			}
 		}
 	}
-	
+
+	/**
+	 * Constructor for copying another board
+	 * @param board The board to base the copy on
+	 */
 	public Board(Board board) {
-		this.fields = new HashMap<Position, Field>();
+		this.fields = new HashMap<>();
 		for(int y = 0; y < MAX_Y; y++) {
 			for(int x = 0; x < MAX_X; x++) {
 				Position newPos = new Position(x, y);
@@ -30,11 +43,20 @@ public class Board {
 			}
 		}
 	}
-	
+
+	/**
+	 * Gets the field at the given position via a {@link Position} object
+	 * @param pos The position to get the field for
+	 * @return The field at the specified position
+	 */
 	public Field getField (Position pos) {
 		return this.fields.get(pos);
 	}
-	
+
+	/**
+	 * Gets the list of all fields of the board
+	 * @return The list of all {@link Field} objects of the board
+	 */
 	public Map<Position, Field> getAllFields() {
 		return this.fields;
 	}
