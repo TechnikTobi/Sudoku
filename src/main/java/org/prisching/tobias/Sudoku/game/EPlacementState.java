b9ae16787798d43dee91ff242392702c034b8cf1
Tobias Prisching <tobias.prisching@icloud.com>
2022-02-07 13:29:30 +0100

    Added JavaDoc in EPlacementState.java

--- src/main/java/org/prisching/tobias/Sudoku/game/EPlacementState.java
+++ src/main/java/org/prisching/tobias/Sudoku/game/EPlacementState.java
@@ -1,22 +1,32 @@
 package org.prisching.tobias.Sudoku.game;
 
+/**
+ * This enum describes the result of trying to set the value of a field by a player and the points they get for that
+ */
 public enum EPlacementState {
 
 	CORRECT {
+		/** {@inheritDoc} */
 		@Override public int points() {
 			return 100;
 		}
 	},
 	INCORRECT {
+		/** {@inheritDoc} */
 		@Override public int points() {
 			return -100;
 		}
 	},
 	INVALID {
+		/** {@inheritDoc} */
 		@Override public int points() {
 			return 0;
 		}
 	};
-	
+
+	/**
+	 * Gets the amount of points the player receives for their action
+	 * @return Number of points
+	 */
 	public abstract int points();
 }
