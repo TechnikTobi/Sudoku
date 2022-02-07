c6a68eb8dfac97d00ce6bfbeb6e1ec3e199aa486
Tobias Prisching <tobias.prisching@icloud.com>
2022-02-07 13:29:19 +0100

    Added JavaDoc in EGameState.java

--- src/main/java/org/prisching/tobias/Sudoku/game/EGameState.java
+++ src/main/java/org/prisching/tobias/Sudoku/game/EGameState.java
@@ -1,9 +1,12 @@
 package org.prisching.tobias.Sudoku.game;
 
+/**
+ * This enum describes the different states a game goes through in its lifetime
+ */
 public enum EGameState {
 
 	READY,
 	ONGOING,
-	FINISHED;
+	FINISHED
 	
 }
